1d2ed1062aee3ef85b66a5460e7288e2e4541ee5
JackNel <jcneligan@gmail.com>
2018-05-31 23:36:22 -0400

    Add some styling

--- index.php
+++ index.php
@@ -4,20 +4,21 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Weather Scrapper</title>
+    <link rel="stylesheet" href="styles.css">
+    <title>Weather Scraper</title>
   </head>
   <body>
-  <section class="container">
-    <h1>What's The Weather?</h1>
-    <form>
-      <label>Enter the name of a city.</label>
-      <input type="text"/>
-      <button>Submit</button>
-    </form>
-    <div id="forecast"></div>
-
-
-</section>
+    <section class="container">
+      <h1>What's The Weather?</h1>
+      <form>
+        <div class="form-group">
+          <label for="city">Enter the name of a city.</label>
+          <input type="text" class="form-control" id="city" name="city" placeholder="e.g. New York, Tokyo"/>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+      <div id="forecast"></div>
+    </section>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
