badcb54d4dd9d0208e1d62dfcb5e58e3d4081bed
JackNel <jcneligan@gmail.com>
2018-06-04 18:07:49 -0400

    Add additional error handling

--- index.php
+++ index.php
@@ -1,12 +1,30 @@
 <?php
 if ($_GET["city"]) {
 
+    $weather = "";
+    $error = "";
     $city = str_replace(" ", '', $_GET["city"]);
-    $forecastPage = file_get_contents("https://www.weather-forecast.com/locations/" . $city . "/forecasts/latest");
+    $file_headers = @get_headers("https://www.weather-forecast.com/locations/" . $city . "/forecasts/latest");
+    if ($file_headers[0] == "HTTP/1.1 404 Not Found") {
+        $error = "That city could not be found";
+    } else {
 
-    $pageArray = explode('(1&ndash;3 days)</span><p class="b-forecast__table-description-content"><span class="phrase">', $forecastPage);
-    $secondPageArray = explode('</span></p></td><td colspan="9">', $pageArray[1]);
-    $weather = $secondPageArray[0];
+        $forecastPage = file_get_contents("https://www.weather-forecast.com/locations/" . $city . "/forecasts/latest");
+
+        $pageArray = explode('(1&ndash;3 days)</span><p class="b-forecast__table-description-content"><span class="phrase">', $forecastPage);
+
+        if (sizeof($pageArray) > 1) {
+            $secondPageArray = explode('</span></p></td><td colspan="9">', $pageArray[1]);
+
+            if (sizeof($secondPageArray) > 1) {
+                $weather = $secondPageArray[0];
+            } else {
+                $error = "That city could not be found";
+            }
+        } else {
+            $error = "That city could not be found";
+        }
+    }
 }
 ?>
 <!doctype html>
@@ -28,8 +46,11 @@ if ($_GET["city"]) {
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
       </form>
-      <div id="forecast"><?php if ($weather) {
+      <div id="forecast"><?php
+if ($weather) {
     echo '<div class="alert alert-success" role="alert">' . $weather . '</div>';
+} else if ($error) {
+    echo '<div class="alert alert-danger" role="alert">' . $error . '</div>';
 }
 ?></div>
     </section>
